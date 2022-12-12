0a303872258fa517428ad6332c4f1fc5e33d06ec
tim5758 <tim0972178753@gmail.com>
2022-12-12 21:38:41 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/tim/springbootmall/dao/ProductDao.java
+++ src/main/java/com/tim/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.tim.springbootmall.dao;
 import com.tim.springbootmall.dto.ProductRequest;
 import com.tim.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
