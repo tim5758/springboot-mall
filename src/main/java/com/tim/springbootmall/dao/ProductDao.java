814a12a2f1a18df3e0998a84b595a3232d49b374
tim5758 <tim0972178753@gmail.com>
2022-12-13 18:34:47 +0800

    商品功能 - 改善參數傳遞

--- src/main/java/com/tim/springbootmall/dao/ProductDao.java
+++ src/main/java/com/tim/springbootmall/dao/ProductDao.java
@@ -1,6 +1,6 @@
 package com.tim.springbootmall.dao;
 
-import com.tim.springbootmall.constant.ProductCategory;
+import com.tim.springbootmall.dto.ProductQueryParams;
 import com.tim.springbootmall.dto.ProductRequest;
 import com.tim.springbootmall.model.Product;
 
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ProductDao {
 
-    List<Product> getProducts(ProductCategory category, String search);
+    List<Product> getProducts(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
