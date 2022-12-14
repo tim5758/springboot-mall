917508a11b91164f065f3f59742c4f4a0d504a40
tim5758 <tim0972178753@gmail.com>
2022-12-14 16:17:03 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總筆數)

--- src/main/java/com/tim/springbootmall/service/ProductService.java
+++ src/main/java/com/tim/springbootmall/service/ProductService.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public interface ProductService {
 
+    Integer countProduct(ProductQueryParams productQueryParams);
     List<Product> getProducts(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
     void deleteProductById(Integer productId);
-
 }
