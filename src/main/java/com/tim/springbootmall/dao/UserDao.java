d9b7df83ae7e34e7eee23b089333eadc5e37341a
tim5758 <tim0972178753@gmail.com>
2022-12-23 21:05:27 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/tim/springbootmall/dao/UserDao.java
+++ src/main/java/com/tim/springbootmall/dao/UserDao.java
@@ -6,5 +6,6 @@ import com.tim.springbootmall.model.User;
 public interface UserDao {
 
     User getUserById(Integer userId);
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
--- src/main/java/com/tim/springbootmall/dto/UserRegisterRequest.java
+++ src/main/java/com/tim/springbootmall/dto/UserRegisterRequest.java
@@ -1,11 +1,13 @@
 package com.tim.springbootmall.dto;
 
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
     @NotBlank
     private String password;
